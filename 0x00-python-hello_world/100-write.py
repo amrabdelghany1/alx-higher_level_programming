2b8b60fa1cf8a382931a1ac32b0a0499d4577698
Amr Abdelghany <joo_amr91@yahoo.com>
2023-11-28 11:16:15 -0800

    done

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import sys
-    Msg = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-
+msg = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(msg)
-
 sys.exit(1)
 
