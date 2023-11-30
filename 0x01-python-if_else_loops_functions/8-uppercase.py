cabc5f99d0108f33809423f0ae5e5aac46c2133c
Amr Abdelghany <joo_amr91@yahoo.com>
2023-11-29 17:22:37 -0800

    done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if char <= 'a' or char <= 'z':
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
+    print()
