85fa086f2a15c33e229e58f9a5d791b08e46e5f8
Amr Abdelghany <joo_amr91@yahoo.com>
2023-12-03 16:35:28 -0800

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
         print("{} arguments.".formar(len(argv[0])))
     else:
         if argv == 1:
-            print("{} argument: ".format(len(argv) - 1 ))
-            print("{} : {}".format(len(argv) - 1, argv[0]))
+            print("{} argument: ".format(len(int(argv)) - 1 ))
+            print("{} : {}".format(len(int(argv)) - 1, int(argv[0])))
         else:
-            print("{} arguments:".format(len(argv) - 1))
-            for i in range(1, len(argv) - 1):
-                print("{}: {}".format(i, argv[i]))
+            print("{} arguments:".format(len(int(argv)) - 1))
+            for i in range(1, len(int(argv)) - 1):
+                print("{}: {}".format(i, int(argv[i])))
