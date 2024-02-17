46be9a4593ddadefb6176f88fe124fc0d18741fc
Amr Abdelghany <joo_amr91@yahoo.com>
2024-02-16 19:40:16 -0800

    done

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Import the database dump of hbtn_0d_tvshows to your MySQL server.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+Join show_genres
+LEFT JOIN tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC AND tv_show_genre.genre_id;
