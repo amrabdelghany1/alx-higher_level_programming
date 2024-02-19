61f40d0399c8f47761d6d1a9d9c6e84aaa885bc5
Amr Abdelghany <joo_amr91@yahoo.com>
2024-02-18 19:32:32 -0800

    done

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 Join show_genres
 LEFT JOIN tv_shows.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC AND tv_show_genre.genre_id;
+ORDER BY tv_shows.title ASC AND tv_show_genre.genre_id ASC;
