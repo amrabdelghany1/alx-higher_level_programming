553a740ac24f1543194c2d17245cda8ab510d6b2
Amr Abdelghany <joo_amr91@yahoo.com>
2024-02-18 19:41:51 -0800

    done

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,7 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genre
-ON tv_shows.id = tv_show_genre
+LEFT JOIN tv_show_genre
+ON tv_shows.id = tv_show_genre.show_id
+WHERE tv_show_genre.show_id IS NULL
 ORDER BY tv_shows_title ASC, tv_shows_genre.genre_id ASC;
