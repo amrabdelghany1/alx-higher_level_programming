8e2ef90e1db339350de532d4203202e85c221010
Amr Abdelghany <joo_amr91@yahoo.com>
2024-02-18 19:47:24 -0800

    done

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server.
+-- List all rows of a DB
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_show.title ASC AND tv_show_genre.genre_id ASC;
